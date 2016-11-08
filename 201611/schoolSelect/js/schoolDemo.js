4fee7af4d33f78f85bbf44e81286cf4315785d91
kevin-story <792300489@qq.com>
2016-11-09 01:10:46 +0800

    将学校下拉框整出来

--- 201611/schoolSelect/js/schoolDemo.js
+++ 201611/schoolSelect/js/schoolDemo.js
@@ -12,14 +12,25 @@
         $scope.school.dropBox = false;
 
         //当前
+        $scope.tab={};
         $scope.current=1;
-        $scope.setCurrent = function (tab) {
+        $scope.tab.setCurrent = function (tab) {
             $scope.current = tab;
         };
-
-        $scope.isSet = function(tab) {
+        $scope.tab.isSet = function(tab) {
             return $scope.current == tab;
          };
+
+        //地区
+        $scope.provinces=[{id:100000,name:"北京"},{id:110000,name:"天津"},{id:120000,name:"河北"}
+            ,{id:130000,name:"黑龙江"},{id:140000,name:"辽宁"},{id:150000,name:"河南"}
+            ,{id:160000,name:"山西"},{id:170000,name:"山东"},{id:180000,name:"江苏"}];
+        $scope.citys=[{id:100000,name:"北京市"},{id:100000,name:"天津市"},{id:100000,name:"石家庄"}
+            ,{id:130000,name:"黑龙江"},{id:130000,name:"铁林"},{id:130000,name:"郑州"}
+            ,{id:130000,name:"太原"},{id:130000,name:"济南"},{id:130000,name:"南京"}];
+        $scope.towns=[{id:100000,name:"北京市"},{id:100000,name:"天津市"},{id:100000,name:"石家庄"}
+            ,{id:130000,name:"黑龙江"},{id:130000,name:"铁林"},{id:130000,name:"郑州"}
+            ,{id:130000,name:"太原"},{id:130000,name:"济南"},{id:130000,name:"南京"}];
     }
 
     //方法声明
