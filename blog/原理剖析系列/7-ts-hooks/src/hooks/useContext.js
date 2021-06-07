7a6879fa784f789f7e5f1f59f0ece7d1066d095f
guimeisang <guimeisang@users.noreply.github.com>
2021-06-07 23:54:36 +0800

    add: useMemo and useRef

---
+++ blog/原理剖析系列/7-ts-hooks/src/hooks/useContext.js
@@ -0,0 +1,37 @@
+import React, { useContext } from 'react'
+
+const themes = {
+  light: {
+    foreground: '#000',
+    background: '#eee'
+  },
+  dark: {
+    foreground: '#fff',
+    background: '#222'
+  }
+}
+
+const ThemeContext = React.createContext(themes.light)
+
+function App() {
+  return (
+    <ThemeContext.Provider value={themes.dark}>
+      <Toolbar/>
+    </ThemeContext.Provider>
+  )
+}
+
+function Toolbar(props) {
+  return (<div>
+    <ThemedButton />
+  </div>)
+}
+
+function ThemedButton() {
+  const theme = useContext(ThemeContext)
+  return (
+    <button style={{ background: theme.background, color: theme.foreground }}>
+      I am styled by theme context!
+    </button>
+  )
+}
--- blog/原理剖析系列/7-ts-hooks/src/hooks/useMemo.js
+++
@@ -1,3 +0,0 @@
-import { useMemo } from "react";
-
-const memoizedValue = useMemo(() => computeExpensiveValue(a, b), [a, b])
